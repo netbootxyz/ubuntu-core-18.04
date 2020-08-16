a39e0666cc0f704774d4313f39d64f28f4be6532
thelamer <ryankuba@gmail.com>
2020-08-16 07:48:12 -0700

    update version ingestion

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
-URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso.torrent"
-TYPE=torrent
+URL="http://releases.ubuntu.com/18.04/ubuntu-REPLACE_VERSION-desktop-amd64.iso"
+TYPE=file
 CONTENTS="\
 casper/initrd|initrd
 casper/vmlinuz|vmlinuz"
